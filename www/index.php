522f7929c7387cb2526ad206634a25340e0111b9
Serrjik <uac.sarge@gmail.com>
2019-01-29 16:52:21 +0300

    registration-system - first commit

--- www/index.php
+++ www/index.php
@@ -22,6 +22,37 @@ switch ( $uri[0] ) {
 		include "modules/main/index.php";
 		break;
 
+	// ::::::::::::::::::::: USERS :::::::::::::::::::::
+
+	case 'login':
+		require ROOT . "modules/login/login.php";
+		break;
+
+	case 'registration':
+		require ROOT . "modules/login/registration.php";
+		break;
+
+	case 'logout':
+		require ROOT . "modules/login/logout.php";
+		break;
+
+	case 'lost-password':
+		require ROOT . "modules/login/lost-password.php";
+		break;
+
+	case 'set-new-password':
+		require ROOT . "modules/login/set-new-password.php";
+		break;
+
+	case 'profile':
+		require ROOT . "modules/profile/index.php";
+		break;
+
+	case 'profile-edit':
+		require ROOT . "modules/profile/edit.php";
+		break;
+	// :::::::
+
 	case 'about':
 		include "modules/about/index.php";
 		break;
