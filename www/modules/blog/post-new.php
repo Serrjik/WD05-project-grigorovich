265338ab772ed4f476bebfcfc071f8748e13e6ff
Serrjik <uac.sarge@gmail.com>
2019-02-06 01:00:02 +0300

    blog - 3-rd commit --- posts output - ready

--- www/modules/blog/post-new.php
+++ www/modules/blog/post-new.php
@@ -3,7 +3,7 @@
 $title = "Блог - Добавить новый пост";
 
 if ( isset($_POST['postNew']) ) {
-	
+
 	if ( trim($_POST['postTitle']) == '' ) {
 		$errors[] = ['title' => 'Введите Название поста' ];
 	}
@@ -13,7 +13,7 @@ if ( isset($_POST['postNew']) ) {
 	}
 
 	if ( empty($errors) ) {
-		
+
 		$post = R::dispense( 'posts' );
 		$post->title = htmlentities(trim($_POST['postTitle']));
 		$post->text = trim($_POST['postText']);
@@ -31,73 +31,72 @@ if ( isset($_POST['postNew']) ) {
 			$kaboom = explode(".", $fileName);
 			$fileExt = end($kaboom);
 
-			// Check file properties on different conditions
-			list($width, $height) = getimagesize($fileTmpLoc);
-			if ( $width < 10 || $height < 10 ) {
-				$errors[] = ['title' => 'Изображение не имеет размеров. Загрузите изображение побольше.' ];
-			}
-
-			if ( $fileSize > 2097152 ) {
-				$errors[] = ['title' => 'Файл изображения не должен быть более 2 Мбайт' ];
-			}
-
 			if ( !preg_match("/\.(gif|jpg|jpeg|png)$/i", $fileName) ) {
 				$errors[] = [
 					'title' => 'Неверный формат файла', 
 					'desc' => '<p>Файл изображения должен быть в формате gif, jpg, jpeg, или png.</p>' 
 				];
-			}
-
-			if ( $fileErrorMsg == 1 ) {
+			} else if ( $fileSize > 2097152 ) {
+				$errors[] = ['title' => 'Файл изображения не должен быть более 2 Мбайт' ];
+			} else if ( $fileErrorMsg == 1 ) {
 				$errors[] = ['title' => 'При загрузке изображения произошла ошибка. Повторите попытку' ];
+			} else {
+				// Check file properties on different conditions
+				list($width, $height) = getimagesize($fileTmpLoc);
+				if ( $width < 10 || $height < 10 ) {
+					$errors[] = ['title' => 'Изображение не имеет размеров. Загрузите изображение побольше.' ];
+				}
+			}  
+
+			if ( empty($errors) ) {
+
+				// Перемещаем загруженный файл в нужную директорию
+				// Для имени файла генерируется случайное число
+				$db_file_name = rand(100000000000, 999999999999) . "." . $fileExt; // 9989234798.jpg
+				$postImgFolderLocation = ROOT . 'usercontent/blog/'; // usercontent/blog/9989234798.jpg
+				$uploadfile = $postImgFolderLocation . $db_file_name;
+				$moveResult = move_uploaded_file($fileTmpLoc, $uploadfile);
+
+				if ( $moveResult != true ) {
+					$errors[] = ['title' => 'Ошибка сохранения файла' ];
+				}
+
+				require_once(ROOT . "libs/image_resize_imagick.php");
+
+				// Создаем большую картинку
+				$target_file = $postImgFolderLocation . $db_file_name;
+				$wmax = 920;
+				$hmax = 620;
+				$img = createThumbnailCrop($target_file, $wmax, $hmax);
+				$img->writeImage($target_file);
+				// Пишем в БД имя большой картинки
+				$post->postImg = $db_file_name;
+
+				// Создаем маленькую картинку
+				$target_file = $postImgFolderLocation . $db_file_name;
+				$resized_file = $postImgFolderLocation . "320-" . $db_file_name;
+				$wmax = 320;
+				$hmax = 140;
+				$img = createThumbnailCrop($target_file, $wmax, $hmax);
+				$img->writeImage($resized_file);
+				// Пишем в БД имя маленькой картинки
+				$post->postImgSmall = "320-" . $db_file_name;
+
 			}
-
-			// Перемещаем загруженный файл в нужную директорию
-			// Для имени файла генерируется случайное число
-			$db_file_name = rand(100000000000, 999999999999) . "." . $fileExt; // 9989234798.jpg
-			$postImgFolderLocation = ROOT . 'usercontent/blog/'; // usercontent/blog/9989234798.jpg
-			$uploadfile = $postImgFolderLocation . $db_file_name;
-			$moveResult = move_uploaded_file($fileTmpLoc, $uploadfile);
-
-			if ( $moveResult != true ) {
-				$errors[] = ['title' => 'Ошибка сохранения файла' ];
-			}
-
-			require_once(ROOT . "libs/image_resize_imagick.php");
-
-			// Создаем большую картинку
-			$target_file = $postImgFolderLocation . $db_file_name;
-			$wmax = 920;
-			$hmax = 620;
-			$img = createThumbnail($target_file, $wmax, $hmax);
-			$img->writeImage($target_file);
-			// Пишем в БД имя большой картинки
-			$post->postImg = $db_file_name;
-
-			// Создаем маленькую картинку
-			$target_file = $postImgFolderLocation . $db_file_name;
-			$resized_file = $postImgFolderLocation . "320-" . $db_file_name;
-			$wmax = 320;
-			$hmax = 140;
-			$img = createThumbnail($target_file, $wmax, $hmax);
-			$img->writeImage($resized_file);
-			// Пишем в БД имя маленькой картинки
-			$post->postImgSmall = "320-" . $db_file_name;
-
-
-
-
 
 		}
 
-		R::store( $post );
-		header('Location: ' . HOST . 'blog');
-		exit();
+		if ( empty($errors) ) {
+
+			R::store( $post );
+			header('Location: ' . HOST . 'blog');
+			// exit();
+
+		}
 
 	}
 
 }
-
 
 // Готовим контент для центральной части
 ob_start();
